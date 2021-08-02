6f9aa766e0174e00f9a7e904f4e258ec2df27596
Jabah Gundy <gundy157@outlook.com>
2021-08-02 15:59:37 -0400

    FirstCommit

---
+++ src/components/NewMessageForm.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+const NewMessageForm = ({ onSend }) => {
+    const [inputText, setInputText] = useState('');
+
+    const handleChange = (e) => {
+        setInputText(e.target.value);
+    }
+
+    const handleSend = (e) => {
+        e.preventDefault();
+        onSend(inputText);
+        setInputText('')
+    }
+
+
+    return (
+        <div>
+            <input type="text" data-testid='messageText' value={inputText}
+                onChange={(e) => handleChange(e)}
+            />
+            <button type="button" data-testid='sendButton'
+                onClick={(e) => handleSend(e)}
+            > send </button>
+        </div>
+    )
+}
+
+export default NewMessageForm;
